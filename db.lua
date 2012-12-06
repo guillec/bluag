64b345ab1ceb114fd45b29d4df8bc18ae838594b
Guille Carlos <ramon.g.carlos@gmail.com>
2012-12-06 01:59:49 -0500

    Initial commit

---
+++ db.lua
@@ -0,0 +1,5 @@
+entry {
+  title = "Hello",
+  Description = "The description"
+
+}
---
+++ main.lua
@@ -0,0 +1,35 @@
+function fwrite(fmt, ...)
+  return io.write(string.format(fmt, ...))
+end
+
+function writeheader()
+  io.write([[
+  <html>
+    <head><title>Your Blog</title></head>
+    <body>
+  ]])
+end
+
+function entry1(o)
+  count = count + 1
+  local title = o.title or 'none'
+  fwrite('<li><a href="#%d">%s</a>\n', count, title)
+end
+
+function writetail()
+  fwrite('</body></html>\n')
+end
+
+local inputfile = 'db.lua'
+writeheader()
+
+count=0
+f = loadfile(inputfile)
+
+entry = entry1
+fwrite('<ul>\n')
+f()
+fwrite('</ul>\n')
+
+writetail()
+
