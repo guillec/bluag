81d7b16d246098efade32a7221b5e689db7121ea
Guille Carlos <ramon.g.carlos@gmail.com>
2012-12-18 02:05:28 -0500

    To the point of getting data from the db file and printing HTML to the browser

--- posts/index.lua
+++ posts/index.lua
@@ -1,58 +1,26 @@
 require "xavante.cgiluahandler"
 
-
-
-function fwrite(fmt, ...)
-  return io.write(string.format(fmt, ...))
-end
-
-function writeheader()
-  io.write([[
-  <html>
-    <head><title>Your Blog</title></head>
-    <body>
-  ]])
+function fwrite(fmt)
+  print(fmt)
+  return fmt
 end
 
 function entry1(o)
   count = count + 1
   local title = o.title or 'none'
-  fwrite('<li><a href="#%d">%s</a>\n', count, title)
-end
-
-function writetail()
-  fwrite('</body></html>\n')
+  fwrite('<li><a href="#%d">' .. title .. '</a>\n')
 end
 
 local inputfile = '../db.lua'
-writeheader()
+
 
 count=0
 f = loadfile(inputfile)
 
 entry = entry1
-fwrite('<ul>\n')
-f()
-fwrite('</ul>\n')
-writetail()
-
-function fulldocument()
-
-end
-
-local file = io.open("example.html", "w")
-file:write("Hellllloo")
-file:close()
 
 cgilua.htmlheader()
-cgilua.put([[
-<html>
-<head>
-  <title>Hello World</title>
-</head>
-<body>
-  <strong>Hello World!</strong>
-</body>
-</html>
-]])
+cgilua.put("<html><body>")
+cgilua.put(f())
+cgilua.put("</body></html>")
 
