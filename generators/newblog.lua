d8f91681b2295c85a2eb3b70225f29c532dac847
Guille Carlos <guille@bitpop.in>
2013-02-25 00:35:29 -0500

    New index file creation

--- generators/newblog.lua
+++ generators/newblog.lua
@@ -1,19 +1,20 @@
-function mkdir(dirname)
-  if not file_exists(dirname) then
-    os.execute("mkdir " .. dirname)
-  end
-end
 
 function file_exists(name)
   local f=io.open(name,"w")
   if f~=nil then io.close(f) return true else return false end
 end
 
-function create_index_page(filename)
-  file = io.open(filename, "w")
-  file:write("<b>HELLO WORLD THIS IS YOUR NEW BLOG</b>")
-  file:close()
+function mkdir(dirname)
+    os.execute("mkdir " .. dirname)
 end
 
-mkdir("_source")
-create_index_page("index.html")
+function create_index_page(filename)
+  if not file_exists(filename) then
+    file = io.open(filename, "w")
+    file:write("<b>HELLO WORLD THIS IS YOUR NEW BLOG</b>")
+    file:close()
+  end
+end
+
+mkdir("_source/")
+create_index_page("_source/index.html")
