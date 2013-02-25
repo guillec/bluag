ca8cc9d9fac3ec95b751a0a8f1aaa77316deaafb
Guille Carlos <guille@bitpop.in>
2013-02-25 00:05:06 -0500

    Change index file

--- generators/newblog.lua
+++ generators/newblog.lua
@@ -10,7 +10,7 @@ function file_exists(name)
 end
 
 function create_index_page(filename)
-  local file = io.open(filename, "w")
+  file = io.open(filename, "w")
   file:write("<b>HELLO WORLD THIS IS YOUR NEW BLOG</b>")
   file:close()
 end
